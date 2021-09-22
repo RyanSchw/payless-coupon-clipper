7724c3449a886ec658bf24c508c2a98b8ec9428e
Ryan Schwartz <32225075+RyanSchw@users.noreply.github.com>
2021-09-21 22:50:17 -0700

    Start on websocket and puppeteer pages

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const runPuppeteer = require('./payless')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+const expressWs = require('express-ws')(app);
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/testws', (req, res) => res.render('pages/testws'))
+app.get('/screenshot', runPuppeteer)
+
+app.ws('/', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(msg)
+    if (msg == 'status') {
+      ws.send(JSON.stringify({ status: 'ok' }));
+    } else if (msg.startsWith('{')) {
+      const parsed = JSON.parse(msg);
+      if ('start' in parsed) {
+        (async () => {
+          for (let i = 0; i <= 100; i += 10) {
+            await new Promise((res, rej) => { setTimeout(res, 1000) });
+            console.log(i);
+            ws.send(JSON.stringify({ status: 'ok', progress: i }));
+          }
+        })()
+      }
+    } else {
+      ws.send(JSON.stringify({ status: 'error' }));
+    }
+  });
+  console.log('something connected!');
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
---
+++ public/ws.js
@@ -0,0 +1,55 @@
+// Create WebSocket connection.
+const socket = new WebSocket('ws://localhost:5000');
+
+// Connection opened
+socket.addEventListener('open', function (event) {
+    socket.send('status');
+});
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+    const parsed = JSON.parse(event.data);
+    console.log(parsed);
+    if ('status' in parsed) {
+        document.getElementById('loading').style['display'] = 'none';
+        if (parsed['status'] == 'ok') {
+            document.getElementById('whole-form').style['display'] = '';
+            document.getElementById('something-wrong').style['display'] = 'none';
+        } else {
+            document.getElementById('whole-form').style['display'] = 'none';
+            document.getElementById('something-wrong').style['display'] = '';
+        }
+    }
+    if ('progress' in parsed) {
+        const val = parsed['progress'];
+        if (val == 100) {
+            document.getElementById('progress-success').style['display'] = '';
+            document.getElementById('progress-encapsulate').style['display'] = 'none';
+        } else {
+            document.getElementById('progress-bar').setAttribute('aria-valuenow', val);
+            document.getElementById('progress-bar').setAttribute('style',`width:${val}%`);
+        }
+    }
+});
+
+function sendSubmit() {
+    console.log(document.getElementById('email-input').value);
+    document.getElementById('email-input').setAttribute('disabled', true);
+    document.getElementById('pwd-input').setAttribute('disabled', true);
+    document.getElementById('recent-only').setAttribute('disabled', true);
+    document.getElementById('send-submit').setAttribute('disabled', true);
+    document.getElementById('send-submit').classList.add('disabled');
+
+    document.getElementById('progress-encapsulate').style['display'] = '';
+
+    const user = document.getElementById('email-input').value;
+    const pwd = document.getElementById('pwd-input').value;
+    const recentOnly = document.getElementById('recent-only').value == 'on';
+    const json = JSON.stringify({
+        start: true,
+        user,
+        pwd,
+        recentOnly,
+    })
+    socket.send(json)
+}
