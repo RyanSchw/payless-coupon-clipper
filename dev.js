37b8feeaeba0f8c94112f584ebbd2e7828cbb842
Ryan Schwartz <32225075+RyanSchw@users.noreply.github.com>
2021-09-22 23:18:28 -0700

    Get coupon clipping in a workable state

--- dev.js
+++ dev.js
@@ -6,9 +6,11 @@ const password = '';
 const headless = false;
 const MAX_COUPONS = 150;
 
+const recentOnly = true;
+
 let couponsClipped = 0;
 
-const filtersToEnable = ['In Ad', 'Dairy', 'Deli', 'Dry Grocery', 'Fresh Meat', 'Frozen Foods', 'Packaged Meat', 'Snacks & Soft Drinks'];
+const filtersToEnable = ['Beverages', 'Canned-\\&\\ Packaged', 'Condiment-\\&\\ Sauces'];
 const ignoreKeywords = ['coffee', 'creamer', 'almond', 'pizza', 'gum', 'pods', 'nut'];
 
 // https://advancedweb.hu/how-to-use-async-functions-with-array-filter-in-javascript/#async-filter-with-map
@@ -17,11 +19,39 @@ const asyncFilter = async (arr, predicate) => {
     return arr.filter((_v, index) => results[index]);
 }
 
+const delay = async (time_ms) => {
+    return new Promise((res, rej) => setTimeout(res, time_ms))
+}
+
+const textContainsKeywords = (text) => {
+    return ignoreKeywords.some(keyword => text.includes(keyword))
+}
+
+// https://stackoverflow.com/questions/51529332/puppeteer-scroll-down-until-you-cant-anymore
+async function autoScroll(page){
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+}
+
 (async () => {
     puppeteer.use(StealthPlugin())
     const browser = await puppeteer.launch({ 
         headless,
-        slowMo: 400,
+        slowMo: 250,
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
     })
     const page = await browser.newPage()
@@ -51,46 +81,65 @@ const asyncFilter = async (arr, predicate) => {
     const clippedNumber = await clippedElement.evaluate(node => node.innerText);
     couponsClipped = parseInt(clippedNumber);
 
-    // // filter by category
+    console.log(couponsClipped);
 
-    // await page.waitForSelector('.mperks-checkbox__container')
-    // const filters = await page.$$('.mperks-checkbox__container');
+    // filter by category
 
-    // // console.log(await filters[0].evaluate(node => node.innerText)) // Show Clipped Coupons
-    // // now just need to filter out by the innertext for all filters and then click the ones that are valid
+    if (recentOnly) {
+        await page.waitForSelector('#new-coupons-filer')
+        await page.click('#new-coupons-filer')
+    }
 
-    // const filtersToClick = await asyncFilter(filters, async (eleHand) => {
-    //     const txt = await eleHand.evaluate(node => node.innerText)
-    //     return filtersToEnable.some(e => txt.startsWith(e));
-    // });
+    // shop in store
+    await page.waitForSelector('#SearchableList-item-In-Store')
+    await page.click('#SearchableList-item-In-Store')
     
-    // for (const filter of filtersToClick) {
-    //     const checkbox = await filter.$('span');
-    //     await checkbox.click();
-    // }
+    const departmentSearchBox = await page.$('[placeholder="Search Departments"]')
+    for (const filterSuffix of filtersToEnable) {
+        // await departmentSearchBox.click({ clickCount: 3 })
+        await departmentSearchBox.type(filterSuffix.substring(0, 3))
+        const filter = `#SearchableList-item-${filterSuffix}`;
+        await page.waitForSelector(filter)
+        await page.click(filter)
+        await delay(500)
+    }
 
-    // const couponTiles = await page.$$('.coupon-tile__container');
+    const numberOfCouponsElement = await page.$('.CouponCount');
+    const expectedAmountToClip = (await numberOfCouponsElement.evaluate(node => node.innerText)).substring(0, 3).trim();
+    console.log('Expect to clip ', expectedAmountToClip)
+    
+    await autoScroll(page)
 
-    // for (const coupon of couponTiles) {
-    //     if (couponsClipped >= maxCouponsClipped - 1) { break; }
-    //     const txtBox = await coupon.$('.coupon-tile__desc-line-clamp__text');
-    //     const txt = await txtBox.evaluate(node => node.innerText);
+    const couponTiles = await page.$$('li > .kds-Card');
 
-    //     if (ignoreKeywords.some(key => txt.toLowerCase().includes(key))) {
-    //         console.log('filtered', txt)
-    //         continue;
-    //     }
+    console.log('Total cards found ', couponTiles.length)
 
-    //     try {
-    //         // could be coupon-tile__button--unclip if already clipped, so ignore and move on
-    //         const clipButton = await coupon.$('.coupon-tile__button--clip');
-    //         await clipButton.click();
-    //         couponsClipped += 1;
-    //         console.log('clipped', txt, '- total', couponsClipped);
-    //     } catch (e) {}
-    // }
+    for (const coupon of couponTiles) {
+        if (couponsClipped >= MAX_COUPONS - couponsClipped - 1) { break; }
+        try {
+            // could be coupon-tile__button--unclip if already clipped, so ignore and move on
+            // kds-Button kds-Button--primary kds-Button--compact CouponActionButton shadow-4 hover:shadow:2 CouponCard-button ml-8 false w-1/2 body-m font-500
+            // kds-Button kds-Button--cancel kds-Button--compact CouponActionButton CouponCard-button ml-8 false w-1/2 body-m font-500
+            const clipButton = await coupon.$('.kds-Button--primary');
+            const text = await coupon.$eval('.kds-Button--primary', node => node.getAttribute('aria-label'))
+            // console.log(text)
 
-    // // await page.waitForSelector('#asdf') // timeout
+            if (textContainsKeywords(text.toLowerCase())) {
+                console.log('filtered', text)
+                continue;
+            } else {
+                await clipButton.click();
+                couponsClipped += 1;
+                console.log('clipped', text, '- total', couponsClipped);
+            }
+        } catch (e) {
+            console.log(e)
+        }
+        // break;
+    }
+
+    // await page.waitForSelector('#asdf') // timeout
+    await delay(5000)
 
     await browser.close()
 })().catch(e => {console.error(e)})
